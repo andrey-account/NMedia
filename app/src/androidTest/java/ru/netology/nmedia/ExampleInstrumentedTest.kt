399f1df44007442c022a557c501fdfe7b946a03c
Andrey B <andreychitry@mail.ru>
2023-01-10 23:06:42 +0300

    add text, buttons

--- app/src/androidTest/java/ru/netology/nmedia/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/netology/nmedia/ExampleInstrumentedTest.kt
@@ -21,4 +21,154 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("ru.netology.nmedia", appContext.packageName)
     }
+
+    @Test
+    fun heartTen() {
+        heartClickCount = 10 //Значение счётчика, с которым тестируется функция
+        val expected = "11" //Функция heartIncrement() увеличит счётчик на 1 и преобразует число в строку
+        val actual = heartIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun heartThousand() {
+        heartClickCount = 1000 //Значение счётчика, с которым тестируется функция
+        val expected = "1K" //Функция heartIncrement() увеличит счётчик на 1 и преобразует число в строку
+        val actual = heartIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun heartThousandAndHundred() {
+        heartClickCount = 1100 //Значение счётчика, с которым тестируется функция
+        val expected = "1.1K" //Функция heartIncrement() увеличит счётчик на 1 и преобразует число в строку
+        val actual = heartIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun heartTenThousand() {
+        heartClickCount = 10_000 //Значение счётчика, с которым тестируется функция
+        val expected = "10K" //Функция heartIncrement() увеличит счётчик на 1 и преобразует число в строку
+        val actual = heartIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun heartMillion() {
+        heartClickCount = 1_000_000 //Значение счётчика, с которым тестируется функция
+        val expected = "1M" //Функция heartIncrement() увеличит счётчик на 1 и преобразует число в строку
+        val actual = heartIncrement()
+        assertEquals(expected, actual)
+    }
+    @Test
+    fun heartMillionAndThousand() {
+        heartClickCount = 1_100_000 //Значение счётчика, с которым тестируется функция
+        val expected = "1.1M" //Функция heartIncrement() увеличит счётчик на 1 и преобразует число в строку
+        val actual = heartIncrement()
+        assertEquals(expected, actual)
+    }
+
+
+//---------------------------------------------------------------------------------------------
+
+//Tests for fun shareIncrement
+    @Test
+    fun shareTen() {
+        shareClickCount = 10
+        val expected = "11"
+        val actual = shareIncrement()
+        assertEquals(expected, actual)
+    }
+    @Test
+    fun shareThousand() {
+        shareClickCount = 1000
+        val expected = "1K"
+        val actual = shareIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shareThousandAndHundred() {
+        shareClickCount = 1100
+        val expected = "1.1K"
+        val actual = shareIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shareTenThousand() {
+        shareClickCount = 10_000
+        val expected = "10K"
+        val actual = shareIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shareMillion() {
+        shareClickCount = 1_000_000
+        val expected = "1M"
+        val actual = shareIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shareMillionAndThousand() {
+        shareClickCount = 1_100_000
+        val expected = "1.1M"
+        val actual = shareIncrement()
+        assertEquals(expected, actual)
+    }
+
+
+
+//---------------------------------------------------------------------------------------------
+
+// Tests for fun lookIncrement
+    @Test
+    fun lookTen() {
+        lookClickCount = 10
+        val expected = "11"
+        val actual = lookIncrement()
+        assertEquals(expected, actual)
+    }
+    @Test
+    fun lookThousand() {
+        lookClickCount = 1000
+        val expected = "1K"
+        val actual = lookIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun lookThousandAndHundred() {
+        lookClickCount = 1100
+        val expected = "1.1K"
+        val actual = lookIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun lookTenThousand() {
+        lookClickCount = 10_000
+        val expected = "10K"
+        val actual = lookIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun lookMillion() {
+        lookClickCount = 1_000_000
+        val expected = "1M"
+        val actual = lookIncrement()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun lookMillionAndThousand() {
+        lookClickCount = 1_100_000
+        val expected = "1.1M"
+        val actual = lookIncrement()
+        assertEquals(expected, actual)
+    }
 }
--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,11 +1,138 @@
 package ru.netology.nmedia
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.ImageButton
+import android.widget.TextView
+
+import kotlin.math.roundToInt
+
+var heartClickCount = 1_099_998 //Счётчик лайков
+var shareClickCount = 1_099_998 //Счётчик репостов
+var lookClickCount = 1_099_998 //Счётчик просмотров
+const val POSITIVE_INFINITY = 1.0/0.0 //Верхний предел счётчиков
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.textView)
+        textView.movementMethod = LinkMovementMethod.getInstance()
+
+        val heartButton =
+            findViewById<ImageButton>(R.id.heartImageButton) //Тип параметра=кнопка с картинкой, id = heartImageButton
+        val printLikes: TextView = findViewById(R.id.heartTextView) //Вывод результата в TextView
+        heartButton.setOnClickListener {
+            printLikes.text = heartIncrement()
+        } //После нажатия вызывает функцию heartIncrement()
+
+        val shareButton =
+            findViewById<ImageButton>(R.id.shareImageButton) //Тип параметра=кнопка с картинкой, id = shareImageButton
+        val printShares: TextView = findViewById(R.id.shareTextView) //Вывод результата в TextView
+        shareButton.setOnClickListener {
+            printShares.text = shareIncrement()
+        } //После нажатия вызывает функцию shareIncrement()
+
+        val lookButton =
+            findViewById<ImageButton>(R.id.lookImageButton) //Тип параметра=кнопка с картинкой, id = shareImageButton
+        val printLooks: TextView = findViewById(R.id.lookTextView) //Вывод результата в TextView
+        lookButton.setOnClickListener {
+            printLooks.text = lookIncrement()
+        } //После нажатия вызывает функцию shareIncrement()
+    }
+}
+
+
+fun heartIncrement(): String {
+    heartClickCount++
+    when (heartClickCount) {
+        in 1000..1_099 -> {
+            val text = (heartClickCount / 1000) //Выводит 1K лайков
+            return text.toString() + "K"
+        }
+        in 1100..9_999 -> {
+            var text = (heartClickCount.toDouble() / 1000) //Выводит 1.1K лайков
+            text = (text * 10).roundToInt() / 10.0 // делить надо обязательно на тип Double
+            return text.toString() + "K"
+        }
+        in 10_000..999_999 -> {
+            val text = (heartClickCount / 1000) //Выводит 10K лайков
+            return text.toString() + "K"
+        }
+        in 1_000_000..1_099_999 -> {
+            val text = (heartClickCount / 1_000_000) //Выводит 1M лайков
+            return text.toString() + "M"
+        }
+        in 1_100_000..POSITIVE_INFINITY.toInt() -> {
+            var text =
+                (heartClickCount.toDouble() / 1_000_000) //Выводит 1.1 лайков
+            text = (text * 10).roundToInt() / 10.0 // делить надо обязательно на тип Double
+            return text.toString() + "M"
+        }
+        else -> return heartClickCount.toString() //Преобразование Int в строку
+    }
+}
+
+
+fun shareIncrement(): String {
+    shareClickCount++
+    when (shareClickCount) {
+        in 1000..1_099 -> {
+            val text = (heartClickCount / 1000)
+            return text.toString() + "K"
+        }
+        in 1100..9_999 -> {
+            var text = (heartClickCount.toDouble() / 1000)
+            text = (text * 10).roundToInt() / 10.0
+            return text.toString() + "K"
+        }
+        in 10_000..999_999 -> {
+            val text = (heartClickCount / 1000)
+            return text.toString() + "K"
+        }
+        in 1_000_000..1_099_999 -> {
+            val text = (shareClickCount / 1_000_000) //Выводит количество миллионов репостов
+            return text.toString() + "M"
+        }
+        in 1_100_000..POSITIVE_INFINITY.toInt() -> {
+            var text =
+                (heartClickCount.toDouble() / 1_000_000) //Выводит 1.1 репостов
+            text = (text * 10).roundToInt() / 10.0 // делить надо обязательно на тип Double
+            return text.toString() + "M"
+        }
+        else -> return shareClickCount.toString() //Преобразование Int в строку
+    }
+}
+
+
+fun lookIncrement(): String { //Просмотры
+    lookClickCount++
+    when (lookClickCount) {
+        in 1000..1_099 -> {
+            val text = (heartClickCount / 1000)
+            return text.toString() + "K"
+        }
+        in 1100..9_999 -> {
+            var text = (heartClickCount.toDouble() / 1000)
+            text = (text * 10).roundToInt() / 10.0
+            return text.toString() + "K"
+        }
+        in 10_000..999_999 -> {
+            val text = (heartClickCount / 1000)
+            return text.toString() + "K"
+        }
+        in 1_000_000..1_099_999 -> {
+            val text = (lookClickCount / 1_000_000)
+            return text.toString() + "M"
+        }
+        in 1_100_000..POSITIVE_INFINITY.toInt() -> {
+            var text = (lookClickCount.toDouble() / 1_000_000)
+            text = (text * 10).roundToInt() / 10.0
+            return text.toString() + "M"
+        }
+        else -> return lookClickCount.toString()
     }
 }

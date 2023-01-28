a065a1de07498fd30b0859ead5a0f7173eae6bf3
Andrey B <andreychitry@mail.ru>
2023-01-28 23:25:07 +0300

    "CRUD"

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 <h2>For block kotlin developing for android</h2>
 <a href="https://github.com/netology-code/and2-homeworks">About</a>
 
-<h2>«2.3. Отображение списков: RecyclerView»</h2>
+<h2>«2.4. CRUD: списки, добавление, удаление, изменение»</h2>
--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -2,29 +2,93 @@ package ru.netology.nmedia.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import ru.netology.nmedia.databinding.ActivityMainBinding //Сгенерированный автоматически java класс
 import androidx.activity.viewModels
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostsAdapter
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostsAdapter(
-            {
-                viewModel.likeById(it.id)
-            }, {
-                viewModel.share(it.id)
-            }, {
-                viewModel.look(it.id)
-            })
+
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                binding.editGroup.visibility = View.VISIBLE
+                viewModel.edit(post)
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                viewModel.share(post.id)
+            }
+
+            override fun onLook(post: Post) {
+                viewModel.look(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+        })
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
+        }
+
+        viewModel.edited.observe(this) { post ->
+            if (post.id == 0L) {
+                return@observe
+            }
+            with(binding.content) {
+                requestFocus()
+                setText(post.content)
+            }
+        }
+        binding.notEdit.setOnClickListener {
+            with(binding.content) {
+                if (text.isNotEmpty()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        context.getString(R.string.notEdit),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    text.clear()
+                    binding.editGroup.visibility = View.GONE //Перестаёт занимать место на экране
+                    AndroidUtils.hideKeyboard(this)
+                }
+            }
+        }
+
+        binding.save.setOnClickListener {
+            with(binding.content) {
+                if (text.isNullOrBlank()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        context.getString(R.string.error_empty_content),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+
+                viewModel.changeContent(text.toString())
+                viewModel.save()
+
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+            }
         }
     }
 }

f5b498fa45119fba789fcc888b3390dc89f40b74
Andrey B <andreychitry@mail.ru>
2023-02-07 18:38:51 +0300

    "Add files to git"

---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostViewHolder.kt
@@ -0,0 +1,66 @@
+package ru.netology.nmedia.adapter
+
+import android.view.View
+import androidx.appcompat.widget.PopupMenu
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.likeText
+
+class PostViewHolder(
+    private val binding: CardPostBinding,
+    private val onInteractionListener: OnInteractionListener
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(post: Post) {
+        binding.apply {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+
+            menu.setOnClickListener {
+                PopupMenu(it.context, it).apply {
+                    inflate(R.menu.options_post)
+                    setOnMenuItemClickListener { item ->
+                        when (item.itemId) {
+                            R.id.remove -> {
+                                onInteractionListener.onRemove(post)
+                                true
+                            }
+                            R.id.edit -> {
+                                onInteractionListener.onEdit(post)
+                                true
+                            }
+                            else -> false
+                        }
+                    }
+                }.show()
+            }
+
+            if (post.video != "") videoPreview.visibility = View.VISIBLE
+            else videoPreview.visibility = View.GONE
+            videoPreview.setOnClickListener {
+                onInteractionListener.onPlay(post)
+            }
+
+
+            like.isChecked = post.likedByMe
+            like.text = likeText(post.likeClickCount)
+            like.setOnClickListener {
+                onInteractionListener.onLike(post)
+            }
+
+
+            share.text = likeText(post.shareClickCount)
+            share.setOnClickListener {
+                onInteractionListener.onShare(post) //with(onShareListener){onShare(post)}
+            }
+
+
+            look.setOnClickListener {
+                onInteractionListener.onLook(post) //onLookListener(post)
+            }
+            lookTextView.text = likeText(post.lookClickCount)
+        }
+    }
+}
